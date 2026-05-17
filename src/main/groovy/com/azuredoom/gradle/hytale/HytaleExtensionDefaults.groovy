30c159e8aec9f4116e6d360e1b6faa3b7490640a
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2026-05-17 18:00:24 -0400

    Add TODO to do post May 26 update

--- src/main/groovy/com/azuredoom/gradle/hytale/HytaleExtensionDefaults.groovy
+++ src/main/groovy/com/azuredoom/gradle/hytale/HytaleExtensionDefaults.groovy
@@ -7,6 +7,7 @@ final class HytaleExtensionDefaults {
 
 	static void apply(Project project, HytaleExtension ext) {
 		ext.javaVersion.convention(project.providers.gradleProperty('java_version').map { (it ?: '25') as Integer }.orElse(25))
+		// TODO: Make default '0.+' instead of '2026.+' after next update
 		ext.hytaleVersion.convention(project.providers.gradleProperty('hytale_version').orElse('2026.+'))
 		ext.patchline.convention(project.providers.gradleProperty('hytale_patchline').orElse('release'))
 		ext.oauthBaseUrl.convention(project.providers.gradleProperty('hytools.hytale.oauth.base').orElse('https://oauth.accounts.hytale.com'))
